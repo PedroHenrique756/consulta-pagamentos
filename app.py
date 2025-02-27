24b4ec9694aadb05db08b65c9d4cb47cad8f1e75
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-26 21:45:59 -0300

    Entra no verificador de cpf

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ drive = webdriver.Chrome()
 drive.get('https://consultcpf-devaprender.netlify.app/')
 for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
     nome, valor, cpf, vencimento = linha
+    # Entra no verificador de cpf
+    sleep(5)
+    campo_pesquisa = drive.find_element(By.XPATH, "//input[@id='cpfInput']")
+    sleep(1)
+    campo_pesquisa.send_keys(cpf)
