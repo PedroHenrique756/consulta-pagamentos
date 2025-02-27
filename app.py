cf1163ae5b2bc13a81591e2d073ef5aa3d8fbfb1
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-02-26 21:46:11 -0300

    Verificar se está "em dia" ou "atrasado"

--- app.py
+++ app.py
@@ -16,3 +16,16 @@ for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
     campo_pesquisa = drive.find_element(By.XPATH, "//input[@id='cpfInput']")
     sleep(1)
     campo_pesquisa.send_keys(cpf)
+    # Verificar se está "em dia" ou "atrasado"
+    btn_pesquisar = drive.find_element(By.XPATH, "//button[@class='btn btn-custom btn-lg btn-block mt-3']")
+    sleep(1)
+    btn_pesquisar.click()
+    sleep(4)
+
+    status = drive.find_element(By.XPATH, "//span[@id='statusLabel']")
+    if status.text == 'em dia':
+        # Se estiver em dia, pegar a data do pagamento e o método do pagamento (cartão, boleto)
+        data_pagamento = drive.find_element(By.XPATH, "//p[@id='paymentDate']")
+        metodo_pagamento = drive.find_element(By.XPATH, "//p[@id='paymentMethod']")
+    else:
+        # Caso contrario (se estiver atrasado) coloque o status como pendente
